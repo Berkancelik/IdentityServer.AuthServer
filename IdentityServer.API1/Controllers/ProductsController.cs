c412f902853c8ee79593d7cb72ede19845f4f489
Berkan Çelik <berkan.celik.190515@gmail.com>
2022-05-01 12:50:27 +0300

    (Client Credentials)configuration 6

---
+++ IdentityServer.API1/Controllers/ProductsController.cs
@@ -0,0 +1,31 @@
+﻿using IdentityServer.API1.Model;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IdentityServer.API1.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        //
+        [Authorize]
+        [HttpGet]
+        public IActionResult GetProducts()
+        {
+            var productList = new List<Product>()
+            {
+                new Product{Id = 1, Name = "Kalem", Price = 100, Stock = 500},
+                new Product{Id = 2, Name = "Silgi", Price = 100, Stock = 500},
+                new Product{Id = 3, Name = "Defter", Price = 100, Stock = 500},
+                new Product{Id = 4, Name = "Kitap", Price = 100, Stock = 500},
+                new Product{Id = 5, Name = "Bant", Price = 100, Stock = 500}
+            };
+            return Ok(productList);
+        }
+    }
+}
---
+++ IdentityServer.API2/Controllers/PicturesController.cs
@@ -0,0 +1,29 @@
+﻿using IdentityServer.API2.Model;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IdentityServer.API2.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class PicturesController : ControllerBase
+    {
+        [Authorize]
+        [HttpGet]
+        public IActionResult GetPictures()
+        {
+            var pictures = new List<Picture>()
+            {
+                new Picture{Id = 1, Name = "Mono Lisa",Url="monolisa.jpg" },
+                new Picture{Id = 1, Name = "Son Akşam Yemeği",Url="sonaksamyemegi.jpg" },   
+                new Picture{Id = 2, Name = "Fareli Köyün Kavalcısı",Url="farelikoyunkavalcisi.jpg" },
+           
+            };
+            return Ok(pictures);
+        }
+    }
+}
