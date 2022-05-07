4bf83774cf6494fbc055043ab56da0f9e8f7a4e8
Berkan Çelik <79005929+Berkancelik@users.noreply.github.com>
2022-05-07 10:35:08 +0300

    Update Program.cs
    test

--- IdentityServer.API2/Program.cs
+++ IdentityServer.API2/Program.cs
@@ -14,7 +14,7 @@ namespace IdentityServer.API2
         public static void Main(string[] args)
         {
             CreateHostBuilder(args).Build().Run();
-        }
+        } 
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
