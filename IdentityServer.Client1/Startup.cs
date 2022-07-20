5cb655364225985da577a0e0354534b31b874ec7
Berkan Çelik <79005929+Berkancelik@users.noreply.github.com>
2022-07-20 12:18:13 +0300

    Cookie settings have been made for Mvc Client 1

--- IdentityServer.Client1/Startup.cs
+++ IdentityServer.Client1/Startup.cs
@@ -20,13 +20,28 @@ namespace IdentityServer.Client1
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
+ 
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddAuthentication(opts =>
+            {
+
+                opts.DefaultScheme = "Cookies";
+                opts.DefaultChallengeScheme = "oidc";
+            }).AddCookie("Cookies").AddOpenIdConnect("oidc", opts =>
+            {
+                opts.SignInScheme = "Cookies";
+                opts.Authority = "https://localhost:5001";
+                opts.ClientId = "Client1-Mvc";
+                opts.ClientSecret = "secret";
+                opts.ResponseType = "code id_token";
+            });
+
+
+
             services.AddControllersWithViews();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
@@ -36,7 +51,7 @@ namespace IdentityServer.Client1
             else
             {
                 app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+               
                 app.UseHsts();
             }
             app.UseHttpsRedirection();
