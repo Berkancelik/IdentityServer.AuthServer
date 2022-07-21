e90135f14f2a24ecd29a61a921f72f85bfd12ef5
Berkan Çelik <79005929+Berkancelik@users.noreply.github.com>
2022-07-21 22:21:31 +0300

    Successfully getting a new token with refresh token

--- IdentityServer.Client1/Controllers/UserController.cs
+++ IdentityServer.Client1/Controllers/UserController.cs
@@ -1,16 +1,28 @@
-﻿using Microsoft.AspNetCore.Authentication;
+﻿using IdentityModel.Client;
+using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Protocols.OpenIdConnect;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
 using System.Threading.Tasks;
 
 namespace IdentityServer.Client1.Controllers
 {
     public class UserController : Controller
     {
+        private readonly IConfiguration _configuration;
+
+        public UserController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
         [Authorize]
         public IActionResult Index()
         {
-            
+
             return View();
         }
 
@@ -20,5 +32,50 @@ namespace IdentityServer.Client1.Controllers
 
             await HttpContext.SignOutAsync("oidc");
         }
+
+        public async Task<IActionResult> GetRefreshToken()
+        {
+            HttpClient httpClient = new HttpClient();
+            var disco = await httpClient.GetDiscoveryDocumentAsync("https://localhost:5001");
+
+            if (disco.IsError)
+            {
+                //loglama yap
+            }
+
+            var refreshToken = await HttpContext.GetTokenAsync(OpenIdConnectParameterNames.RefreshToken);
+
+            RefreshTokenRequest refreshTokenRequest = new RefreshTokenRequest();
+            refreshTokenRequest.ClientId = _configuration["Client1Mvc:ClientId"];
+            refreshTokenRequest.ClientSecret = _configuration["Client1Mvc:ClientSecret"];
+            refreshTokenRequest.RefreshToken = refreshToken;
+            refreshTokenRequest.Address = disco.TokenEndpoint;
+
+            var token = await httpClient.RequestRefreshTokenAsync(refreshTokenRequest);
+
+            if (token.IsError)
+            {
+                //yönlendirme yap
+            }
+
+            var tokens = new List<AuthenticationToken>()
+            {
+                new AuthenticationToken{Name = OpenIdConnectParameterNames.IdToken, Value=token.IdentityToken},
+                new AuthenticationToken(){Name = OpenIdConnectParameterNames.RefreshToken, Value=token.RefreshToken},
+                new AuthenticationToken{Name = OpenIdConnectParameterNames.RefreshToken,Value = token.RefreshToken},
+                new AuthenticationToken{Name = OpenIdConnectParameterNames.ExpiresIn,Value = DateTime.UtcNow.AddSeconds(token.ExpiresIn).ToString("o") }
+            };
+
+            var authenticationResult = await HttpContext.AuthenticateAsync();
+
+            var properties = authenticationResult.Properties;
+
+            properties.StoreTokens(tokens);
+
+            await HttpContext.SignInAsync("Cookies", authenticationResult.Principal, properties);
+
+            return RedirectToAction("Index");
+        }
+
     }
 }
