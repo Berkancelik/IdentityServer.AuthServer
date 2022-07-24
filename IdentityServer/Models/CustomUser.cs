cd493ea82090764c724ed54e1b55385cf449a23d
Berkan Çelik <79005929+Berkancelik@users.noreply.github.com>
2022-07-24 13:01:28 +0300

    Custom User class has been created

---
+++ IdentityServer/Models/CustomUser.cs
@@ -0,0 +1,10 @@
+﻿namespace IdentityServer.AuthServer.Models
+{
+    public class CustomUser
+    {
+        public int Id { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string City { get; set; }
+    }
+}
