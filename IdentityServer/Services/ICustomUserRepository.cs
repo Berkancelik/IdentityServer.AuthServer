ad2feceaadebe1fdd4d8ea22a6abf3828f8dc7c7
Berkan Çelik <79005929+Berkancelik@users.noreply.github.com>
2022-07-24 13:48:41 +0300

    ICustomUserRepository has been created

---
+++ IdentityServer/Services/ICustomUserRepository.cs
@@ -0,0 +1,13 @@
+﻿using IdentityServer.AuthServer.Models;
+using System.Threading.Tasks;
+
+namespace IdentityServer.AuthServer.Services
+{
+    public interface ICustomUserRepository
+    {
+        Task<bool> Validate(string email, string password);
+
+        Task<CustomUser> FindById(int id);
+        Task<CustomUser> FindByEmail(string email);
+    }
+}
