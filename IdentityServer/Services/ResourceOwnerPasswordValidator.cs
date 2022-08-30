4879e7d7c4e66c437466f2f13eabb471822d95a2
Berkan Çelik <79005929+Berkancelik@users.noreply.github.com>
2022-08-30 20:20:42 +0300

    ResourceOwnerPasswordValidator has been created

---
+++ IdentityServer/Services/ResourceOwnerPasswordValidator.cs
@@ -0,0 +1,13 @@
+﻿using IdentityServer4.Validation;
+using System.Threading.Tasks;
+
+namespace IdentityServer.AuthServer.Services
+{
+    public class ResourceOwnerPasswordValidator : IResourceOwnerPasswordValidator
+    {
+        public Task ValidateAsync(ResourceOwnerPasswordValidationContext context)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
