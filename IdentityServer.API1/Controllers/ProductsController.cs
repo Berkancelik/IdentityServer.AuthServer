e16c92b831bf0787d01792ca9dc5e2b5345beb9a
Berkan Çelik <79005929+Berkancelik@users.noreply.github.com>
2022-09-17 17:28:46 +0300

    release branch created

--- IdentityServer.API1/Controllers/ProductsController.cs
+++ IdentityServer.API1/Controllers/ProductsController.cs
@@ -1,10 +1,7 @@
 ﻿using IdentityServer.API1.Model;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace IdentityServer.API1.Controllers
 {
